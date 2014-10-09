eba839cb7f46124a0e23213a3844b4c7620bdece
Jmcus <vluxyue@gmail.com>
2014-10-09 13:48:00 +0800

    linearmodel

---
+++ linearmodel.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sys
+
+N = int(sys.argv[1])
+
+c = np.loadtxt('data.csv',delimiter = ',', usecols = (6,),unpack = True)
+b = c[-N:]
+b = b[::-1]
+print "b", b
+
+A = np.zeros((N,N),float)
+print "Zeros N by N", A
+
+for i in range(N):
+    A[i, ] = c[-N -1 -i: -1 -i]
+
+print "A",A
+
+(x, residuals, rank, s) = np.linalg.lstsq(A,b) #估计线性模型中的系数
+
+print x,residuals,rank,s
+
+print np.dot(b,x)
