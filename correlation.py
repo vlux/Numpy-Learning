9e4ae322a7b8041db3b6c3dcc0cfb0e5461f3fe5
Jmcus <vluxyue@gmail.com>
2014-10-09 15:22:35 +0800

    correlation

---
+++ correlation.py
@@ -0,0 +1,29 @@
+import numpy as np
+from matplotlib.pyplot import plot
+from matplotlib.pyplot import show
+
+bhp = np.loadtxt('BHP.csv',delimiter = ',', usecols = (6,),unpack = True)
+bhp_returns = np.diff(bhp) / bhp[:-1]
+
+vale = np.loadtxt('VALE.csv',delimiter = ',', usecols = (6,),unpack = True)
+vale_returns = np.diff(vale) / vale[:-1]
+
+covariance = np.cov(bhp_returns,vale_returns)
+print "Covariance", covariance
+
+print "Covariance diagonal", covariance.diagonal()    #查看对角线上的元素
+print "Covariance trace", covariance.trace()    #计算矩阵的迹，即对角线上元素之和
+
+print covariance / (bhp_returns.std() * vale_returns.std())
+print "Correlation coefficient", np.corrcoef(bhp_returns, vale_returns)
+
+difference = bhp - vale
+avg = np.mean(difference)
+dev = np.std(difference)
+
+print "Out of sync",np.abs(difference[-1] - avg) > 2 * dev
+
+t = np.arange(len(bhp_returns))
+plot(t, bhp_returns, lw = 1)
+plot(t, vale_returns, lw = 2)
+show()
